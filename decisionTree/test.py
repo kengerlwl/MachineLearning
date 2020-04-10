db8c5ba14a6968261aab547c857f960610a543c3
2892211452 <2892211452@qq.com>
2020-04-10 22:54:43 +0800

    决策树

---
+++ decisionTree/test.py
@@ -0,0 +1,9 @@
+dataSet=[    [1,1,'yes'],
+             [1,1,'yes'],
+             [1,0,'no'],
+             [0,1,'no'],
+             [0,1,'no']]
+
+featList=[example[2] for example in dataSet]
+
+print(featList)
